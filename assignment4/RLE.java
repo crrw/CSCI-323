0f36847e2e0495eb3521c884f7fc01c7cb7054b8
crw <47400245+crrw@users.noreply.github.com>
2020-07-23 21:11:18 -0400

    Update RLE.java

--- assignment4/RLE.java
+++ assignment4/RLE.java
@@ -3,7 +3,16 @@ public class RLE{
     public static void main(String[] args){
         String s = "wwwwaaadexxxxxxywww";
         System.out.println(rle(convert(s)));
+        String w = "w12a3d1e1x6y1w3";
+        String d = r(w);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(decomp(d));
+        String test ="wwwwwwwwwwww";
+
+        System.out.println(test.length());
     }
+
     public static String convert(String s){
         return s.replaceAll("\\s+", "");
     }
@@ -31,5 +40,35 @@ public class RLE{
             sb.append(1);
         }
         return sb.toString();
+    }
+
+    public static String r(String s){
+        StringBuilder sb = new StringBuilder(); 
+        if(s.length() == 0) return ""; 
+
+        for(int i=s.length()-1; i>=0; i--){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static String decomp(String s){
+        String num = "";
+        StringBuilder sb = new StringBuilder(); 
+        
+        for(int i=0; i<s.length(); i++){
+            if(Character.isDigit(s.charAt(i))){
+                num += s.charAt(i);
+            }
+            else{
+                StringBuilder t = new StringBuilder(num);
+                num = t.reverse().toString();
+                for(int j=0; j<Integer.parseInt(num); j++){
+                    sb.append(s.charAt(i));
+                }
+                num = "";
+            }
+        }
+        return sb.reverse().toString();
     }
 }
