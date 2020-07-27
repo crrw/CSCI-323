68c041d34aafe3c732bb79595856348f8e4cd0cd
aasish23 <47400245+aasish23@users.noreply.github.com>
2020-07-26 20:56:29 -0400

    wip

--- assignment5/StringSearch.java
+++ assignment5/StringSearch.java
@@ -1,7 +1,18 @@
 import java.util.*;
-
+import java.io.*;
 public class StringSearch{
     public static void main(String[] args){
-        
-    }   
+
+    }
+    public static readFile()throws FileNotFounndException{
+        File file = new File("input1.txt");
+        Scanner sc = new Scanner(file);
+        StringBuilder sb = new StringBuilder();
+
+        while(sc.hasNext()){
+            sb.append(sc.nextLine());
+        }
+
+        return sb.toString();
+    }
 }
