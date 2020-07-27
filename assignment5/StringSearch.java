660fd2c14865ca01ffb0fd5495ab45345ac25925
aasish23 <47400245+aasish23@users.noreply.github.com>
2020-07-26 21:17:41 -0400

    wip

--- assignment5/StringSearch.java
+++ assignment5/StringSearch.java
@@ -1,18 +1,39 @@
 import java.util.*;
 import java.io.*;
-public class StringSearch{
-    public static void main(String[] args){
 
+public class StringSearch{
+    static List<Integer> bfIndex;
+    public static void main(String[] args)throws FileNotFoundException{
+        String s = readFile(1);
+        String findF = "free";
+        String findS = "brave"; 
+        String findT = "nation";
+        bruteForce(s, findT);
+        System.out.print(bfIndex);
     }
-    public static readFile()throws FileNotFounndException{
-        File file = new File("input1.txt");
+    public static String readFile(int n)throws FileNotFoundException{
+        String read = "input"+Integer.toString(n)+".txt";
+        File file = new File(read);
         Scanner sc = new Scanner(file);
         StringBuilder sb = new StringBuilder();
 
         while(sc.hasNext()){
             sb.append(sc.nextLine());
         }
+        return sb.toString().toLowerCase();
+    }
 
-        return sb.toString();
+    public static void bruteForce(String text, String find){
+        bfIndex = new ArrayList();
+        for(int i=0; i<text.length()-find.length()+1; i++){
+            for(int j=0; j<find.length(); j++){
+                if(text.charAt(i+j) != find.charAt(j)){
+                    break;
+                }
+                else if(j == find.length()){
+                    bfIndex.add(i);
+                }
+            }
+        }
     }
 }
