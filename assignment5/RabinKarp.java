a3f6baa243e785107cbf44d4762bc676a840b4a9
crrw <47400245+aasish23@users.noreply.github.com>
2020-07-29 20:48:59 -0400

    done

--- assignment5/RabinKarp.java
+++ assignment5/RabinKarp.java
@@ -6,13 +6,13 @@ public class RabinKarp {
     static Map<Integer, String> searchTextMap;
     static String inputText = "", searchText = "";
     static long time = 0;
-    static int comp = 0, searchIndex = 1, textIndex = 1;
+    static int comparisons = 0, searchIndex = 1, textIndex = 1;
     public final static int uniq = 256;
 
     public static void main(String[] args) throws FileNotFoundException {
         inputTextMap = new HashMap();
         searchTextMap = new HashMap();
-        textIndex = 2;
+        textIndex = 1;
         searchIndex = 1;
         inputTextMap.put(1, "Gettysburg Address");
         inputTextMap.put(2, "Star Spangled Banner");
@@ -23,7 +23,7 @@ public class RabinKarp {
         searchTextMap.put(3, "NATION");
         for (int i = 1; i <= 3; i++) {
             searchText = searchTextMap.get(searchIndex++);
-            rabinKarp(s, searchText, 97);
+            rabinKarp(s, searchText, 101);
         }
 
     }
@@ -44,35 +44,35 @@ public class RabinKarp {
         int M = pattern.length();
         int N = text.length();
         int i, j;
-        int p = 0, t = 0, hash = 1;
+        int ptrn = 0, txt = 0, hash = 1;
 
         for (i = 0; i < M - 1; i++) {
             hash = (hash * uniq) % prime;
         }
 
         for (i = 0; i < M; i++) {
-            p = (uniq * p + pattern.charAt(i)) % prime;
-            t = (uniq * t + text.charAt(i)) % prime;
+            ptrn = (uniq * ptrn + pattern.charAt(i)) % prime;
+            txt = (uniq * txt + text.charAt(i)) % prime;
         }
 
         for (i = 0; i < N - M; i++) {
-            comp++;
-            if (p == t) {
+            comparisons++;
+            if (ptrn == txt) {
                 for (j = 0; j < M; j++) {
                     if (text.charAt(i + j) == pattern.charAt(j)) {
                         break;
                     }
                 }
                 if (j == M) {
-                    Print out = new Print(i, comp, time, searchText, inputText);
+                    Print out = new Print(i, comparisons, time, searchText, inputText);
                     out.print(out);
                     return;
                 }
             }
             if (i < N - M) {
-                t = (uniq * (t - text.charAt(i) * hash) + text.charAt(i + M)) % prime;
-                if (t < 0) {
-                    t = t + prime;
+                txt = (uniq * (txt - text.charAt(i) * hash) + text.charAt(i + M)) % prime;
+                if (txt < 0) {
+                    txt = txt + prime;
                 }
             }
         }
